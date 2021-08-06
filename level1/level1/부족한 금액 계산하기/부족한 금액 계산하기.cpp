a613f28682b29109e49d0f09a89d91d7980e437a
tldmschl81 <chse0810@gmail.com>
2021-08-06 17:34:18 +0900

    level1_부족한 금액 계산하기

---
+++ level1/level1/부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -0,0 +1,29 @@
+﻿using namespace std;
+#include <iostream>
+
+long long solution(int price, int money, int count)
+{
+	long long answer = -1;
+	long long sumMoney = 0;
+
+	for (int i = 1; i <= count; i++) {
+		sumMoney += (price * i);
+	}
+
+	long long finalMoney = money - sumMoney;
+	if (finalMoney < 0)
+		answer = finalMoney * -1;
+
+	else if (finalMoney >= 0)
+		answer = 0;
+	cout << answer;
+	return answer;
+}
+
+int main() {
+	int price = 300;
+	int money = 1600;
+	int count = 6;
+	solution(price, money, count);
+	return 0;
+}
