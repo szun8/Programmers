c46c703f260c13f6746a437fa984fcd553fbff67
tldmschl81 <chse0810@gmail.com>
2021-08-06 17:35:48 +0900

    두 개 뽑아서 더하기

---
+++ level1/level1/두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+	vector<int> answer;
+
+	for (int i = 0; i < numbers.size(); i++) {
+		for (int j = i + 1; numbers.size() - j; j++) {
+			int num = numbers[i] + numbers[j];
+			answer.push_back(num);
+		}
+	}
+
+	sort(answer.begin(), answer.end());
+	answer.erase(unique(answer.begin(), answer.end()), answer.end());
+	return answer;
+}
