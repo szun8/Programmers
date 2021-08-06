6bbd5e4167c904f2396a3676e53f59e285ea6bb5
tldmschl81 <chse0810@gmail.com>
2021-08-06 17:32:22 +0900

    level1_자연수 뒤집어 배열로 만들기

---
+++ level1/level1/자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    string reverseN = to_string(n);
+    for (int i = reverseN.size() - 1; i >= 0; i--) {
+        answer.push_back(reverseN[i] - '0');
+    }
+    return answer;
+}
+
+int main() {
+    long long n = 12345;
+    solution(n);
+    return 0;
+}
