146bd86fdbdfa8418878ae6333c37b81e06cab7f
tldmschl81 <chse0810@gmail.com>
2021-08-07 02:24:22 +0900

    level1_평균 구하기

---
+++ level1/level1/평균 구하기/평균 구하기.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        answer += arr[i];
+    }
+    answer /= arr.size();
+    return answer;
+}
+
+int main() {
+    vector<int> arr = {1,2,3,4};
+    solution(arr);
+    return 0;
+}
