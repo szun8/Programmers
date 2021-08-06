040735a980e608c91baed51b260963b6e7d3e99a
tldmschl81 <chse0810@gmail.com>
2021-08-07 03:33:19 +0900

    level1_수박수박수박수

---
+++ level1/level1/수박수박수박수/수박수박수박수.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    if (n <= 0) return answer;
+    answer.insert(0, "수");
+    // append()
+
+    for (int i = 1; i < n; i++) {
+        if (i % 2 == 0) {
+            answer += "수";
+        }
+        else {
+            answer+="박";
+        }
+    }
+    return answer;
+}
+
+int main() {
+    solution(4);
+    return 0;
+}
