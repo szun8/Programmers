17099f9c5076863204d63c55ae026b1f47aa6d3a
tldmschl81 <chse0810@gmail.com>
2021-08-06 17:31:44 +0900

    level1_핸드폰번호가리기

---
+++ level1/level1/핸드폰번호가리기/핸드폰번호가리기.cpp
@@ -0,0 +1,14 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+	string answer = "";
+
+	for (int i = 0; i < phone_number.size() - 4; i++) {
+		phone_number[i] = '*';
+	}
+
+	return answer = phone_number;
+}
