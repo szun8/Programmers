d79d303a06e15bd08ff3685d9e311ce9d54d104e
tldmschl81 <chse0810@gmail.com>
2021-08-06 17:32:58 +0900

    level1_자릿수 더하기

---
+++ level1/level1/자릿수더하기/자릿수더하기.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int solution(int n) {
+	int answer = 0;
+	string numberStr = to_string(n);
+	for (int i = 0; i < numberStr.size(); i++) {
+		answer += (numberStr[i] - '0');
+	}
+	return answer;
+}
