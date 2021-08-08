af3f16b1b578e64312ab4b3f521ab3df4819aacf
tldmschl81 <chse0810@gmail.com>
2021-08-08 17:58:01 +0900

    level1_가운데 글자 가져오기

---
+++ level1/level1/가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int halfSize = s.size() / 2;
+    if (s.size() % 2 == 1) {    // if Odd,
+        answer.push_back(s[halfSize]);
+    }
+    else {      // if Even,
+        answer.push_back(s[halfSize-1]);
+        answer.push_back(s[halfSize]);
+    }   
+    return answer;
+}
+
+int main() {
+    solution("abcde");
+    solution("qwer");
+    return 0;
+}
