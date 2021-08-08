1baa8321b65f26f34f6e54f0449b7b3675e66969
tldmschl81 <chse0810@gmail.com>
2021-08-08 18:00:45 +0900

    level1_콜라츠 추측

---
+++ level1/level1/콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    long long n = num;
+    int answer = 0;
+    while (n != 1) {  // 1이 될때까지 돌려주기
+        if (answer >= 500)
+            return -1;
+
+        if (n % 2 == 0) {    // if Even,
+            n /= 2;
+            answer++;
+        }
+        else if (n % 2 == 1) {    // if Odd,
+            n = (n * 3) + 1;
+            answer++;
+        }
+    }
+    return answer;
+}
+
+int main() {
+    solution(6);
+    return 0;
+}
