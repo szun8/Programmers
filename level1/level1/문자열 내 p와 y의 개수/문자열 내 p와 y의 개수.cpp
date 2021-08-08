8a2393bcd208b28bcb27df6bc43bfe7af1465deb
tldmschl81 <chse0810@gmail.com>
2021-08-08 17:59:12 +0900

    level1_문자열 내 p와 y의 개수

---
+++ level1/level1/문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int countP = 0;
+    int countY = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'p' || s[i] == 'P')
+            countP++;
+        if (s[i] == 'y' || s[i] == 'Y')
+            countY++;
+    }
+    // 인자로 받은 문자를 대문자 혹 소문자로 만들어서 문제푸는 방법도 가능
+    // tolower() || toupper()
+    // https://blockdmask.tistory.com/452
+    if (countP != countY)
+        answer = false;
+
+    return answer;
+}
