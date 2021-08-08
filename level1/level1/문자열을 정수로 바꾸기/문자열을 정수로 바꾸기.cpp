15b4f5eb5dd012f27db0fa36d32d40e11227254e
tldmschl81 <chse0810@gmail.com>
2021-08-08 17:59:31 +0900

    level1_문자열을 정수로 바꾸기

---
+++ level1/level1/문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,15 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = stoi(s);
+    // 부호까지 처리해주는 string to int ()
+    return answer;
+}
+
+int main() {
+    solution("-1234");
+    return 0;
+}
