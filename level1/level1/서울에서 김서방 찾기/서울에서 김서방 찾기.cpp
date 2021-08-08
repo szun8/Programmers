1737e934b307b663329b24c5a9b3144826be1161
tldmschl81 <chse0810@gmail.com>
2021-08-08 17:59:59 +0900

    level1_서울에서 김서방 찾기

---
+++ level1/level1/서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer ="김서방은 ";
+    for (int i = 0; i < seoul.size(); i++) {
+        if (seoul.at(i) == "Kim") {
+            string num = to_string(i);
+            // 문자열에 숫자를 더해주는 것 X 
+            answer += num;
+            answer += "에 있다";
+        }
+    }
+    return answer;
+}
+
+int main() {
+    vector<string> seoul{"Jane","Kim"};
+    solution(seoul);
+    return 0;
+}
