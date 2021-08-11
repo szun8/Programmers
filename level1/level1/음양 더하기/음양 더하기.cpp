ff3d5cce46dceb440fdda2de0821d4164d0a88b8
tldmschl81 <chse0810@gmail.com>
2021-08-11 20:54:13 +0900

    level1_음양 더하기

---
+++ level1/level1/음양 더하기/음양 더하기.cpp
@@ -0,0 +1,15 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 123456789;
+    int add = 0;
+    for (int i = 0; i < absolutes.size(); i++) {
+        if (signs[i] == false) absolutes[i] *= -1;
+
+        add += absolutes[i];
+    }
+    return answer = add;
+}
