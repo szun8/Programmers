6909dd42dfad5d86c334f1050402d7403d3e370c
tldmschl81 <chse0810@gmail.com>
2021-08-11 20:54:31 +0900

    level1_하샤드 수

---
+++ level1/level1/하샤드 수/하샤드 수.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int y = 0;
+    string num = to_string(x);
+    for (int i = 0; i < num.size(); i++) {
+        y += (num[i] - '0');
+    }
+    if (x % y == 0) answer = true;
+    else answer = false;
+
+    return answer;
+}
