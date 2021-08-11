4f4677bca1c988a54ee9dfbc0921408108f5ffdc
tldmschl81 <chse0810@gmail.com>
2021-08-11 20:53:56 +0900

    level1_정수 제곱근 판별

---
+++ level1/level1/정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    long long num = 1;
+    while (num * num != n) {
+        if (num > n) {
+            answer = -1;
+            return answer;
+        }
+        ++num;
+    }
+    answer = (num + 1) * (num + 1);
+    return answer;
+}
