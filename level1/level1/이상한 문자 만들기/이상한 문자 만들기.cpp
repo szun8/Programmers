0e3e2c4315395f204bd7bdc4da5e6e7f10e899be
tldmschl81 <chse0810@gmail.com>
2021-08-12 16:51:34 +0900

    level1_이상한 문자 만들기

---
+++ level1/level1/이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    for (int z = 0; z < s.size(); z++) {
+        s[z] = tolower(s[z]);
+    }
+    int i = 0;
+    int j = 0;
+    while (i != s.size()) {
+        if (s[i] == 32) {
+            j = -1;
+        }
+        else if (j == 0 || j % 2 == 0) {
+            s[i] = toupper(s[i]);
+        }
+        i++; j++;
+    }
+    
+    return answer = s;
+}
+
+int main() {
+    solution("apple banana couple");
+    return 0;
+}
