080b66037cc7054b3a4e7050834df508de80f896
tldmschl81 <chse0810@gmail.com>
2021-08-15 18:13:14 +0900

    level1_정수 내림차순 배치

---
+++ level1/level1/정수 내림차순 배치/정수 내림차순 배치.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    vector<int> num;
+    for (int i = 0; n > 10; i++) {
+        num.push_back(n % 10);
+        n/= 10;
+    }
+    num.push_back(n);
+    sort(num.begin(), num.end());
+    
+    for (int i = 0; i <num.size(); i++) {
+        answer += num[i]*pow(10, i);
+    }
+    return answer;
+}
+
+int main() {
+    solution(118372);
+    return 0;
+}
