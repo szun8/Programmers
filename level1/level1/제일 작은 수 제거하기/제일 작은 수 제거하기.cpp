2263a630d056c5b1efdadafc5c83474bd716de67
tldmschl81 <chse0810@gmail.com>
2021-08-15 17:44:49 +0900

    level1_제일 작은 수 제거하기

---
+++ level1/level1/제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    if (arr.size() == 1) {
+        answer.push_back(-1);
+        return answer;
+    }
+    int min = *min_element(arr.begin(), arr.end());
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == min)
+            arr.erase(arr.begin() + i, arr.begin() + i + 1);
+    }
+    for (int i = 0; i < arr.size(); i++) {
+        answer.push_back(arr[i]);
+    }
+
+    return answer;
+}
