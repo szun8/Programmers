28d15be1f41034e3e3534da557701e96050e6d04
tldmschl81 <chse0810@gmail.com>
2021-08-15 19:39:03 +0900

    level1_크레인 인형뽑기 게임

---
+++ level1/level1/크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    stack<int> box;
+    while (!moves.empty()) {
+        int pos = moves.front()-1;
+        for (int i = 0; i < board.size() ; ++i) {
+            if (board[i][pos] != 0) {
+                if (!(box.empty()) && box.top() == board[i][pos]) {
+                    answer += 2;
+                    box.pop();
+                    board[i][pos] = 0;
+                    break;
+                }
+                box.push(board[i][pos]);
+                board[i][pos] = 0;
+                break;
+            }
+        }
+        moves.erase(moves.begin());
+    }
+    return answer;
+}
+
+int main() {
+    vector<vector<int>> board = { {0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1} };
+    vector<int> moves = { 1,5,3,5,1,2,1,4 };
+    solution(board, moves);
+    return 0;
+}
