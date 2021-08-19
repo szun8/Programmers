9791ab3fe51944aca7349199fa0ee891a8d46e5d
tldmschl81 <chse0810@gmail.com>
2021-08-19 18:59:52 +0900

    level1_폰켓몬

---
+++ level1/level1/폰켓몬/폰켓몬.cpp
@@ -0,0 +1,24 @@
+﻿#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    int size = nums.size() / 2;
+    sort(nums.begin(), nums.end());
+    // unique()를 써줄 때는 항상 sort() 정렬을 해주자!
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+    if (size < nums.size())
+        answer = size;
+    else if (size >= nums.size())
+        answer = nums.size();
+
+    return answer;
+}
+
+int main() {
+    vector<int> nums = { 1,1 };
+    solution(nums);
+    return 0;
+}
