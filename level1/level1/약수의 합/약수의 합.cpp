5b34dd32f12c61f3d0eb9c8594602618df94d0ab
tldmschl81 <chse0810@gmail.com>
2021-08-19 17:23:26 +0900

    level1_약수의 합

---
+++ level1/level1/약수의 합/약수의 합.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0)
+            answer += i;
+    }
+    return answer;
+}
+
+int main() {
+    cout<<sqrt(3);
+    solution(3);
+    return 0;
+}
