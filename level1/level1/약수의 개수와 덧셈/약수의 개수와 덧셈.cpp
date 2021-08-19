27298ab62a85e7eb57a66b3b6fc83262088d48f6
tldmschl81 <chse0810@gmail.com>
2021-08-19 20:45:40 +0900

    level1_약수의 개수와 덧셈

---
+++ level1/level1/약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    for (int l = left; l <= right; l++) {
+        int count = 0;
+        for (int i = 1; i <= l; i++) {
+            if (l % i == 0)
+                ++count;
+        }
+        if (count % 2 == 0)
+            answer += l;
+        else
+            answer -= l;
+    }
+    return answer;
+}
