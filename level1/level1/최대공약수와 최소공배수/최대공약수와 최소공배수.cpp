da1ff0bd0285c7ba8d624cf1bed84a0659a5631d
tldmschl81 <chse0810@gmail.com>
2021-08-19 19:56:34 +0900

    level1_최대공약수와 최소공배수(correct.ver)

--- level1/level1/최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ level1/level1/최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -2,42 +2,20 @@
 #include <vector>
 
 using namespace std;
-vector<int> max(int n) {
-    vector<int> num;
-    for (int i = 1; i <= n; ++i) {
-        if (n % i == 0) 
-            num.push_back(i);
+int gcd(int n, int m) {
+    int r;
+    while (m != 0) {
+        r = n % m;
+        n = m;
+        m = r;
     }
-    return num;
+    return n;
 }
 
 vector<int> solution(int n, int m) {
     vector<int> answer;
-    vector<int> N = max(n);
-    vector<int> M = max(m);
-
-    // 최대 공약수 찾기!
-    for (int i = 1; i < N.size(); ++i) {
-        for (int j = 1; j < M.size(); ++j) {
-            if (N[i] == M[j]) {
-                answer.push_back(N[i]);
-                i = N.size();
-                break;
-            }  
-        }
-    }
-    // 최대 공약수 없으면, 1 넣기
-    if (answer.empty()) answer.push_back(1);
-
-    //최소 공배수 찾기!
-    if (m % n == 0) {
-        answer.push_back(m);
-    }
-    else if (n % m == 0) {
-        answer.push_back(m);
-    }
-    else
-        answer.push_back(m*n);
+    answer.push_back(gcd(n, m));    //최대공약수
+    answer.push_back(n * m / gcd(n, m));
 
     return answer;
 }
