1617ad625571ca9e1218f5704288a9712e899a99
tldmschl81 <chse0810@gmail.com>
2021-08-19 17:22:03 +0900

    level1_같은 숫자는 싫어

---
+++ level1/level1/같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+    for (int i = 0; i < arr.size(); i++) {
+        if (i < arr.size() - 1 && arr[i] == arr[i + 1]);
+        else 
+            answer.push_back(arr[i]);
+    }
+    return answer;
+}
+
+int main() {
+    vector<int> arr = {1,1,3,3,0,1,1};
+    solution(arr);
+    return 0;
+}
