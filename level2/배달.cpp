bc207fe6bb417100f93a39eefa33e86fb9989afd
최시은 <chse0810@gmail.com>
2021-12-17 19:08:00 +0900

    배달_코드 수정 및 주석 추가

--- level2/배달.cpp
+++ level2/배달.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 bool visited[51];   // false = 미방문, true = 방문
 vector<int> Time;
-
+// 특정마을에서 마을1에 가는 경로를 탐색하는 순으로 조건 검색
 void dfs(vector<pair<int,int> > graph[], int start, int k, int times, int hours){
+    // start = 시작지점, times = 이전마을에서 해당마을로 오기까지의 거리, hours = 시작마을부터 이전마을까지 이동한 거리
     visited[start]=true;
-    int Hours= hours+times;
-    if(Hours>k){
+    int Hours= hours+times; // 시작마을부터 해당마을까지의 이동거리
+    if(Hours>k){    // 배달시간이 요구조건보다 더 걸림
         visited[start]=false;
         return ;
     }
@@ -20,29 +21,29 @@ void dfs(vector<pair<int,int> > graph[], int start, int k, int times, int hours)
         return ;
     }
     for (int i = 0; i < graph[start].size(); i++) {
-        if(Time.size()>0) break;
+        if(Time.size()>0) break;    // 하나라도 갈수있는 경로가 있다면 반복문 빠져나오기
         if(visited[graph[start][i].first]==false) { 
             dfs(graph, graph[start][i].first, k, graph[start][i].second, Hours);
+            // 다음 마을을 갈수있다면, 거기로 가기
         }
     }
     visited[start]=false;
+    // 다른 경우의 수에서 해당마을을 방문할수 있게 한 경우가 끝나면 미방문처리하기
 }
 
 int solution(int N, vector<vector<int> > road, int K) {
     int answer = 0;
     vector<pair<int,int> > graph[N+1]; // 연결된 노드, 가는 시간
-    for (int i = 0; i < 7; i++) { // 무조건 길은 하나로 히고 작은 값이 들어가기
-    // 입력수정하기
+    for (int i = 0; i < road.size(); i++) { 
         graph[road[i][0]].push_back(pair<int,int>(road[i][1],road[i][2]));
         graph[road[i][1]].push_back(pair<int,int>(road[i][0],road[i][2]));
     }
     for (int i = 2; i <= N; i++) {
         memset(visited,false,sizeof(visited));
         Time.clear();
-        dfs(graph,i, K, 0, 0);
+        dfs(graph,i, K, 0, 0); // 마을i에서 마을1 가는 방법 순회 시작
         answer+=Time.size();
     }
-    cout<< answer+1;
     return answer+1;
 }
 
